cf01d6ec079f622dbfdc26420d7ac90ac8c21d49
mahmoudaton33 <mahmoudsayed3395@gmail.com>
2024-10-23 14:32:45 +0300

    update_const_classes

---
+++ const&classes/classes.hpp
@@ -0,0 +1,21 @@
+#ifndef _CLASSES_H_
+#define _CLASSES_H_
+
+#include <string>
+
+
+class Player
+{
+    private:
+    std::string name;
+    int age;
+    static int number;
+
+    public:
+    Player(std::string name , int age);
+    std::string &Get_name(void) ;
+    void Set_Name(std::string name);
+    void Display( Player &pl);
+};
+
+#endif
---
+++ const&classes/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include "classes.hpp"
+
+
+
+
+
+
+int main()
+{
+    Player pl1("ATON" , 29);
+
+    Player pl2("Ahmed" , 2);
+    pl1.Display(pl2);
+}
